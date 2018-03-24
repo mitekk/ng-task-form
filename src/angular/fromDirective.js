28492d314888e64f807275bd13b3e5e1d32fdc35
mitekk <mitekk@gmail.com>
2018-03-24 23:29:24 +0300

    working form example

---
+++ src/angular/fromDirective.js
@@ -0,0 +1,44 @@
+import app from '../../main'
+import '../react-form/main.29095ab3.css';
+
+const formDirective = app.directive('formDirective', function () {
+    return {
+        template: ``,
+        scope: {},
+        link: function (scope, el, attrs) { }
+    }
+})
+
+export default formDirective
+
+
+// import app from '../../main'
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Layout from "../react/Layout";
+
+// const reactDirective = app.directive('reactDirective', function() {
+//   return {
+//       template: '<div id="reactapp" class="react-part"></div>',
+//       scope: {
+//         todos: '=',
+//         markComplete:'&'
+//       },
+//       link: function(scope, el, attrs){
+//             scope.newItem = (value) => {alert (value)}
+//             // scope.markComplete = (todoItem) => {scope.markItemCompleted(todoItem)}
+
+//             const reactapp = document.getElementById('reactapp')
+//             scope.$watch('todos', function(newValue, oldValue) {
+//                 if (angular.isDefined(newValue)) {
+//                  ReactDOM.render(
+//                     <Layout todos={newValue} newItem={scope.newItem} markComplete={scope.markComplete}/>
+//                     , reactapp);
+//                 }
+//             }, true);
+
+//         }
+//     }
+// })
+
+// export default reactDirective
